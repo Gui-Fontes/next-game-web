73ce3307ab822ef3c9e1774e4087341a6af30fbb
Guilherme <fontesguilherme9@gmail.com>
2021-06-25 19:08:31 -0300

    Crianção form

---
+++ nextgameweb/src/Form/Formulario.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./estilo.css";
+
+class Formulario extends Component {
+
+  render() {
+    return (
+       <form>
+           <h1>abab</h1>
+           <input type="text" name="Game"/>
+       </form>
+    );
+  }
+}
+
+export default Formulario;
